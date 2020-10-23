10bff1911d471154181847e332b0d2c32f934496
adifranco2 <adifranco2@gmail.com>
2020-10-22 20:41:28 -0400

    Add project files.

---
+++ musicvideo/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var imgObj = null;
+
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+
+window.onload = init;
